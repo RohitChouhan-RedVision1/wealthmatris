861747a6501e01a3c6655194b6fe748c3f4d18ba
RohitChouhan-RedVision1 <Rohit.Chouhan@redvisionglobal.com>
2025-07-09 12:56:50 +0530

    changes done

--- data/calculators.js
+++ data/calculators.js
@@ -1,17 +1,17 @@
 export const calculators = [
     {
-        title: "Car Planning Calculator",
-        route: "/tools/calculators/car-plan",
-        image: "/images/car.svg"
-    },
-    {
         title: "SIP Calculator",
         route: "/tools/calculators/sip-calculator",
         image: "/images/calculator-form.svg"
     },
      {
-        title: "Retirement Planning Calculator",
-        route: "/tools/calculators/retirement-plan",
+        title: "Step Up SIP Calculator",
+        route: "/tools/calculators/step-calculator",
+        image: "/images/calculator-form.svg"
+    },
+       {
+        title: "Lumpsum Calculator",
+        route: "/tools/calculators/lumpsum-calculator",
         image: "/images/calculator-form.svg"
     },
     {
@@ -19,55 +19,59 @@ export const calculators = [
         route: "/tools/calculators/crorepati-calculator",
         image: "/images/delay.svg"
     },
+     {
+        title: "STP Calculator",
+        route: "/tools/calculators/stp-calculator",
+        image: "/images/calculator-form.svg"
+    },
+     {
+        title: "SWP Calculator",
+        route: "/tools/calculators/swp-calculator",
+        image: "/images/calculator-form.svg"
+    },
+     {
+        title: "Retirement Planning Calculator",
+        route: "/tools/calculators/retirement-plan",
+        image: "/images/calculator-form.svg"
+    },
+    
     {
         title: "Delay Planning Calculator",
         route: "/tools/calculators/delay-plan",
         image: "/images/delay.svg"
     },
-    {
-        title: "House Planning Calculator",
-        route: "/tools/calculators/house-plan",
+      {
+        title: "Life Insurance Calculator",
+        route: "/tools/calculators/life-insurance-plan",
         image: "/images/calculator-form.svg"
     },
-      {
+         {
         title: "Emi Planning Calculator",
         route: "/tools/calculators/emi-plan",
         image: "/images/calculator-form.svg"
     },
+     {
+        title: "Marriage Planning Calculator",
+        route: "/tools/calculators/marriage-plan",
+        image: "/images/wedding-rings.svg"
+    },
+     
     {
         title: "Education Planning Calculator",
         route: "/tools/calculators/education-plan",
         image: "/images/calculator-form.svg"
     },
+  
     {
-        title: "Life Insurance Calculator",
-        route: "/tools/calculators/life-insurance-plan",
+        title: "House Planning Calculator",
+        route: "/tools/calculators/house-plan",
         image: "/images/calculator-form.svg"
     },
-    {
-        title: "Lumpsum Calculator",
-        route: "/tools/calculators/lumpsum-calculator",
-        image: "/images/calculator-form.svg"
-    },
-    {
-        title: "Marriage Planning Calculator",
-        route: "/tools/calculators/marriage-plan",
-        image: "/images/wedding-rings.svg"
-    },
-    {
-        title: "Step Up Calculator",
-        route: "/tools/calculators/step-calculator",
-        image: "/images/calculator-form.svg"
-    },
-    {
-        title: "STP Calculator",
-        route: "/tools/calculators/stp-calculator",
-        image: "/images/calculator-form.svg"
-    },
-    {
-        title: "SWP Calculator",
-        route: "/tools/calculators/swp-calculator",
-        image: "/images/calculator-form.svg"
+
+       {
+        title: "Car Planning Calculator",
+        route: "/tools/calculators/car-plan",
+        image: "/images/car.svg"
     },
     {
         title: "Vacation Planning Calculator", route: "/tools/calculators/vacation-plan",
@@ -78,27 +82,28 @@ export const calculators = [
 export const performance = [
     {
         link: "/performance/sip-performance",
-        title: "SIP Performace",
+        title: "SIP Performance",
         image: "/images/performance.svg"
     },
     {
         link: "/performance/stp-performance",
-        title: "STP Performace",
+        title: "STP Performance",
         image: "/images/performance.svg"
     },
     {
         link: "/performance/swp-performance",
-        title: "SWP Performace",
+        title: "SWP Performance",
+        image: "/images/performance.svg"
+    },
+     {
+        link: "/performance/scheme-performance",
+        title: "Scheme Performance",
         image: "/images/performance.svg"
     },
     {
         link: "/performance/fund-performance",
-        title: "Fund Performace",
+        title: "Fund Performance",
         image: "/images/performance.svg"
     },
-    {
-        link: "/performance/scheme-performance",
-        title: "Scheme Performace",
-        image: "/images/performance.svg"
-    },
+   
 ];
